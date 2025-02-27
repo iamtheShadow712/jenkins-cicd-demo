2bd1317d3542f95deb83bdf627f874185c207a15
iamtheShadow712 <siddharth.krgupta712@gmail.com>
2025-02-28 03:02:10 +0530

    Installed packages and created the express server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+const users = [
+    { id: 1, username: "user 1" },
+    { id: 2, username: "user 2" }
+]
+
+app.use('/api/users', (req, res, next) => {
+    res.status(200).json({
+        data: users
+    })
+})
+
+app.use('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Welcome to the homepage"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('server is running on port: ', PORT)
+})
